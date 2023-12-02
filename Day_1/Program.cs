5b0034108860ab2d07fba29bce1ef5caac64d4e0
mcuellar98 <mcuellar228@gmail.com>
2023-12-02 02:22:48 -0500

    Completed part 1 of Day 1

--- Day_1/Program.cs
+++ Day_1/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Day 1");
+﻿// Part 1
+// string[] lines = File.ReadAllLines("./input.txt");
+// int total = 0;
+// List<string> nums = [];
+
+
+// foreach (string line in lines) {
+//   foreach (char character in line) {
+//     if (Char.IsNumber(character)) {
+//       nums.Add(character.ToString());
+//     }
+//   }
+//   total += int.Parse(nums[0] + nums[^1]);
+//   nums = [];
+// }
+
+//Part 2
+
+class Program {
+  class IndexValueObj
+  {
+      public int Index { get; set; }
+      public string? Value { get; set; }
+  }
+
+  static void Main() {
+    List<IndexValueObj> indicesAndValues = [];
+
+    string[] lines = File.ReadAllLines("./input.txt");
+    int total = 0;
+
+    Dictionary<string, string> numStrings = new()
+    {
+      { "one", "1" },
+      { "two", "2" },
+      { "three", "3" },
+      { "four", "4" },
+      { "five", "5" },
+      { "six", "6" },
+      { "seven", "7" },
+      { "eight", "8" },
+      { "nine", "9" },
+      { "1", "1" },
+      { "2", "2" },
+      { "3", "3" },
+      { "4", "4" },
+      { "5", "5" },
+      { "6", "6" },
+      { "7", "7" },
+      { "8", "8" },
+      { "9", "9" },
+    };
+    int counter = 1;
+    foreach (string line in lines) {
+      Console.WriteLine(counter);
+      counter++;
+      foreach (var key in numStrings.Keys) {
+        int index = line.IndexOf(key);
+        if (index != -1) {
+          indicesAndValues.Add(new IndexValueObj { Index = index, Value = numStrings[key] });
+        }
+      }
+      // Console.WriteLine(getSum(indicesAndValues));
+      total += getSum(indicesAndValues);
+      indicesAndValues = [];
+    }
+    Console.WriteLine(total);
+  }
+
+  static int getSum(List<IndexValueObj> list) {
+    list = [.. list.OrderBy(obj => obj.Index)];
+    return int.Parse(list[0].Value + list[list.Count()-1].Value);
+  }
+}
+
